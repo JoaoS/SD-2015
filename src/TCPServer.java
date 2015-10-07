a78235c32ee79ef6044c46f69a8eceabe3902bc4
Joao Subtil <joaosubtil@BigMac.local>
2015-10-07 14:34:23 +0100

    first commit

---
+++ src/TCPServer.java
@@ -0,0 +1,27 @@
+/**
+ * Created by joaosubtil on 07/10/15.
+ */
+public class TCPServer {
+
+
+
+
+
+    public static void main(String args[]){
+
+
+        System.out.println("porra");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
