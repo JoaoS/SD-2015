c38f81f6dafaec1e3c17a63b5436d78a498af937
Joao Subtil <joaosubtil@BigMac.local>
2015-10-09 14:49:22 +0100

    heartbeat udp done

--- src/TCPServer.java
+++ src/TCPServer.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 public class TCPServer {
 
     private static int maxDropHeartbeats =5;
-    public  static int WAIT=500; //milisseconds response thread wait
+    public  static int WAIT=1000; //milisseconds response thread wait
     private static int udpPort;
 
     private static String firstIP;
@@ -19,8 +19,6 @@ public class TCPServer {
 
 
     public static void main(String args[]){
-
-
 
 
         //read java properties
@@ -58,40 +56,32 @@ public class TCPServer {
 
 
 
-
-
-        try{
-            System.out.println("teste "+InetAddress.getLocalHost().getHostAddress());
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-
         //attempt
         secundaryServer();
 
+        //if not create thread to be primary and respond
+        new PrimaryThread(udpPort).start();
 
     }
 
     public static void secundaryServer()
     {
-
+        int replyNumber=0;
         DatagramSocket aSocket = null;
 
         try {
-            System.out.println(" Connecting as Secundary Server");
+            System.out.println("Connecting as Secundary Server");
             int tries=maxDropHeartbeats;
-            byte [] m=new byte[1000];
-            aSocket = new DatagramSocket();
-
 
             //check my address an ping the other
             String myIP=InetAddress.getLocalHost().getHostAddress();
             String connectIP=myIP.equalsIgnoreCase(firstIP) ? secondIP : firstIP;
-            //System.out.println("vou ligar a "+connectIP);
+            System.out.println("Connecting to "+connectIP);
 
-
+            byte [] m=new byte[1000];
+            aSocket = new DatagramSocket();
             InetAddress aHost = InetAddress.getByName(connectIP);
-            System.out.println("Pinging "+connectIP+" in the port "+udpPort);
+            System.out.println("Pinging "+connectIP+" in the port:"+udpPort);
             DatagramPacket request = new DatagramPacket(m,m.length,aHost,udpPort);
             // System.out.println(" "+request);
             while(tries >0)
@@ -103,16 +93,18 @@ public class TCPServer {
                     byte [] buffer = new byte[1000];
                     DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
                     aSocket.receive(reply);
-                    //System.out.println("Received: " + new String(reply.getData(), 0, reply.getLength()));
-                    tries=5;
+                    System.out.println("Received Reply: " + replyNumber);
+                    replyNumber++;
+                    tries=maxDropHeartbeats;
                     Thread.sleep(WAIT);
                 }catch(SocketTimeoutException tme){
                     //System.err.println(tme);
-                    System.out.println("Primary not found,trying again");
+                    System.out.println("Primary not found,trying again(remaining tries:"+tries+", time interval="+WAIT+"ms)");
                     tries--;
                 } catch (InterruptedException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
+                    System.out.println("Secundary server function");
                 }
             }
         }catch (IOException e1) {
@@ -124,6 +116,35 @@ public class TCPServer {
         }
 
     }
+
+    static class PrimaryThread extends Thread{//will handle backup as primaryTCP
+
+        public  int serverPort;
+
+        PrimaryThread(int s){ serverPort=s;}
+
+        public  void run()
+        {
+            System.out.println("Primary server in port: "+serverPort);
+            DatagramSocket aSocket = null;
+            String s;
+            try{
+                aSocket = new DatagramSocket(serverPort);
+                byte[] buffer = new byte[1000];
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                while(true)//receive packet and respond
+                {
+                    aSocket.receive(request);
+                    DatagramPacket reply = new DatagramPacket(request.getData(),request.getLength(), request.getAddress(), request.getPort());
+                    aSocket.send(reply);
+                }
+            }catch (SocketException e){System.out.println("Socket: " + e.getMessage());
+            }catch (IOException e) {System.out.println("IO: " + e.getMessage());
+            }finally {if(aSocket != null) aSocket.close();}
+        }
+    }
+
+
 
 
 
